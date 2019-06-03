d68e30376c40d6267b2ef9fc634121cd36290aee
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-03 16:45:59 +0300

    refactor

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+go build -o go.out ./go/
+g++ cpp/sum.cpp -o cpp.out
--- time.sh
+++ time.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-time ( ./a.out < inputml ) 2>&1 1>/dev/null
+time ( ./go.out < inputml ) 2>&1 1>/dev/null
+time ( ./cpp.out < inputml ) 2>&1 1>/dev/null
