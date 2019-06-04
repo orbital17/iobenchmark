ba895e44ca15f5958f23b6f0b7b16637323e5a0d
tkachenko <olexiy.tkachenko@gmail.com>
2019-06-04 13:18:08 +0300

    improve go

--- go/main.go
+++ go/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	makeIOAndSolve(readerWriterLocal())
+	solveScanner()
+}
+
+func solveScanner() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	var n, v, sum int32
+	scanner.Scan()
+	n = toInt(scanner.Bytes())
+	for i := 0; i < int(n); i++ {
+		scanner.Scan()
+		v = toInt(scanner.Bytes())
+		sum += v
+	}
+	fmt.Fprintf(os.Stdout, "%d\n", sum)
+}
+
+func toInt(buf []byte) (n int32) {
+	for _, v := range buf {
+		n = n*10 + int32(v-'0')
+	}
+	return
 }
